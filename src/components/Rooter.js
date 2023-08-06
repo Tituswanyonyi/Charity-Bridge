c775b6280c314e7fca7024334ea6ab0721ff91b5
titus <titus.unavailable@student.moringaschool.com>
2023-08-06 21:58:05 +0300

    components and pages created

---
+++ src/components/Rooter.js
@@ -0,0 +1,11 @@
+import '../css/Footer.css';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <p>&copy; 2023 Group 5 CharityBridge. © All Rights Reserved.</p>
+        </footer>
+    );
+};
+
+export default Footer;

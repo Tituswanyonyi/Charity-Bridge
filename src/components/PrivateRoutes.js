eabbc919460bdf65ee21f5061a3f967d603e6880
titus <titus.unavailable@student.moringaschool.com>
2023-08-06 22:17:28 +0300

    PrivateRoutes added

---
+++ src/components/PrivateRoutes.js
@@ -0,0 +1,24 @@
+// PrivateRoute.js
+import React from 'react';
+import { Route, Navigate } from 'react-router-dom';
+
+const PrivateRoute = ({ element: Component, roles, currentUser }) => {
+  // Check if the user is authenticated and authorized
+  const isAuthenticated = !!currentUser;
+  const isAuthorized = roles ? roles.includes(currentUser?.role) : true;
+
+  if (!isAuthenticated) {
+    // Redirect to login page if not authenticated
+    return <Navigate to="/login" />;
+  }
+
+  if (!isAuthorized) {
+    // Redirect to a default unauthorized page or show an error message
+    return <div>Unauthorized Access!</div>;
+  }
+
+  // Render the component if the user is authenticated and authorized
+  return <Route element={<Component />} />;
+};
+
+export default PrivateRoute;

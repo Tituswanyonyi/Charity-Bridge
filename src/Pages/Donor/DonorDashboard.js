3c264672307611a96aecf2f0175341952acf8368
titus <titus.unavailable@student.moringaschool.com>
2023-08-06 22:25:30 +0300

    pages-donordashboard

---
+++ src/Pages/Donor/DonorDashboard.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const DonorDashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to Donor Dashboard</h1>
+      <ul>
+        <li>
+          <Link to="/viewdonationrequests">View Donation Requests</Link>
+        </li>
+        <li>
+          <Link to="/makedonation">Make a Donation</Link>
+        </li>
+        <li>
+        <Link to="/Navbar">View Donation History</Link>
+        </li>
+        <li>
+          <Link to="/logout">Logout</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default DonorDashboard;

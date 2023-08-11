a4c67f4974b9599045410d59ef0dff120112584c
titus <titus.unavailable@student.moringaschool.com>
2023-08-11 14:15:04 +0300

    done

--- src/Pages/Ngo/NgoDashboard.js
+++ src/Pages/Ngo/NgoDashboard.js
@@ -2,28 +2,72 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const NgoDashboard = () => {
+  const containerStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#f2f2f2',
+    padding: '20px',
+  };
+
+  const buttonContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+  };
+
+  const buttonStyle = {
+    textDecoration: 'none',
+    color: 'black',
+    fontWeight: 'bold',
+    display: 'block',
+    padding: '10px 20px',
+    margin: '10px 0',
+    backgroundColor: '#ffffff',
+    borderRadius: '5px',
+    boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+  };
+
+  const imagesContainerStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'flex-end',
+  };
+
+  const imageStyle = {
+    width: '100px',
+    height: 'auto',
+    marginBottom: '20px',
+  };
+
   return (
-    <div style={{ textAlign: 'center', backgroundColor: '#f2f2f2', padding: '20px' }}>
-      <h1 style={{ color: 'blue', fontSize: '24px' }}>Welcome to Ngo Dashboard</h1>
-      <ul style={{ listStyleType: 'none', padding: 0 }}>
-        <li style={{ margin: '10px 0' }}>
-          <Link to="/ngoDonationRequests" style={{ textDecoration: 'none', color: 'black', fontWeight: 'bold' }}>Donation Requests</Link>
-        </li>
-        <li style={{ margin: '10px 0' }}>
-          <Link to="/donationsreceived" style={{ textDecoration: 'none', color: 'black', fontWeight: 'bold' }}>Donations Received</Link>
-        </li>
-        <li style={{ margin: '10px 0' }}>
-          <Link to="/Navbar" style={{ textDecoration: 'none', color: 'black', fontWeight: 'bold' }}>Home</Link>
-        </li>
-        <li style={{ margin: '10px 0' }}>
-          <Link to="/logout" style={{ textDecoration: 'none', color: 'black', fontWeight: 'bold' }}>Logout</Link>
-        </li>
-        <li style={{ margin: '10px 0' }}>
-          <Link to="/DonationRequests" style={{ textDecoration: 'none', color: 'black', fontWeight: 'bold' }}>Donation Requests</Link>
-        </li>
-      </ul>
+    <div style={containerStyle}>
+      <div style={buttonContainerStyle}>
+        <h1 style={{ color: 'blue', fontSize: '24px' }}>Welcome to Ngo Dashboard</h1>
+        <ul style={{ listStyleType: 'none', padding: 0 }}>
+          <li>
+            <Link to="/ngoDonationRequests" style={buttonStyle}>Donation Requests</Link>
+          </li>
+          <li>
+            <Link to="/donationsreceived" style={buttonStyle}>Donations Received</Link>
+          </li>
+          <li>
+            <Link to="/Navbar" style={buttonStyle}>Home</Link>
+          </li>
+          <li>
+            <Link to="/logout" style={buttonStyle}>Logout</Link>
+          </li>
+          <li>
+            <Link to="/DonationRequests" style={buttonStyle}>Donation Requests</Link>
+          </li>
+        </ul>
+      </div>
+      <div style={imagesContainerStyle}>
+        <img src="src/Assets/Images/ngo.png" alt="ngo" style={imageStyle} />
+        <img src="/home/moringa/Development/code/phase-5/Charity-Bridge/src/Assets/Images/envi.jpeg" alt="Image 2" style={imageStyle} />
+      </div>
     </div>
-  );  
+  );
 };
 
 export default NgoDashboard;
